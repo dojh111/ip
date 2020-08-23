42a3e465d83e39bd4f10926fb603b97a3cab2285
dojh111 <dojh111@gmail.com>
2020-08-23 21:51:45 +0800

    Add echo and exit function for Walter and customize chatbot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,50 @@
-public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+import java.util.Scanner;
 
-        //Greet User (Level 0)
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Duke");
+public class Duke {
+    //Prints separator component after text is printed
+    public static void printSeparator() {
+        System.out.println("______________________________________________________________________________");
+    }
+    //Handles commands entered by the user
+    public static void handleUserCommand(String command) {
+        printSeparator();
+        System.out.println(" " + command);
+        printSeparator();
+    }
+    public static void main(String[] args) {
+        String logo = "____    __    ____  ___       __      .___________. _______ .______      \n"
+                + "\\   \\  /  \\  /   / /   \\     |  |     |           ||   ____||   _  \\     \n"
+                + " \\   \\/    \\/   / /  ^  \\    |  |     `---|  |----`|  |__   |  |_)  |    \n"
+                + "  \\            / /  /_\\  \\   |  |         |  |     |   __|  |      /     \n"
+                + "   \\    /\\    / /  _____  \\  |  `----.    |  |     |  |____ |  |\\  \\----.\n"
+                + "    \\__/  \\__/ /__/     \\__\\ |_______|    |__|     |_______|| _| `._____|\n";
+        String endLogo = "________              \n"
+                + "___  __ )____  ______ \n"
+                + "__  __  |_  / / /  _ \\\n"
+                + "_  /_/ /_  /_/ //  __/\n"
+                + "/_____/ _\\__, / \\___/ \n"
+                + "        /____/        ";
+        String userCommand;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Hello from\n" + logo);
+        printSeparator();
+        System.out.println(" Hello! I'm Walter  ◕_◕");
         System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        printSeparator();
+
+        //Loop infinitely until user enters "bye"
+        while(true) {
+            userCommand = in.nextLine();
+            if(userCommand.equals("bye")) {
+                break;
+            }
+            handleUserCommand(userCommand);
+        }
+
+        printSeparator();
+        System.out.println(" I'm sad to see you go ε(´סּ︵סּ`)з. Hope to see you again soon! ／人◕ ‿‿ ◕人＼");
+        System.out.println(endLogo);
+        printSeparator();
     }
 }
