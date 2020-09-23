5da1d96532c7f7ad280883709b6724cd94ef09a3
dojh111 <dojh111@gmail.com>
2020-09-23 18:59:25 +0800

    Update Javadoc for Parser class and refactor methods to include more SLAP

--- src/main/java/walter/Parser.java
+++ src/main/java/walter/Parser.java
@@ -7,6 +7,9 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 
+/**
+ * The Parser class handles text parsing for Walter
+ */
 public class Parser {
     //Exception messages
     public static final String EXCEPTION_EMPTY_FIELD = "Oh no... You have to enter a task number. Please try again!";
@@ -18,12 +21,15 @@ public class Parser {
     public static final String EXCEPTION_EMPTY_TODO = "Oh no! The description of the todo cannot be empty ;-;";
     public static final String EXCEPTION_EMPTY_DATE = "Oh no! The please enter a date in YYYY-MM-DD format!";
     public static final String EXCEPTION_EMPTY_SEARCHTERM = "Oh no! The search term cannot be empty!";
+    public static final String EXCEPTION_CANNOT_DETERMINE_COMMAND = "Oh no, I'm unable to determine the command...";
 
     public static final String BLANK_SPACE = "";
     public static final String WHITESPACE_IDENTIFIER = " ";
     public static final String HYPHEN_IDENTIFIER = "-";
+    public static final int MINIMUM_LENGTH = 2;
 
     public static final int DATE_FORMAT_SIZE = 3;
+    public static final int INVALID_COMMAND_SIZE = 1;
     public static final String DATE_FORMAT = "MMM d yyyy";
 
     /**
@@ -33,7 +39,7 @@ public class Parser {
      * @param commandToRemove Command to be removed from string
      */
     public static String removeCommandFromInput(String userInput, String commandToRemove) {
-        String modifiedUserInput = userInput.replace(commandToRemove, BLANK_SPACE);
+        String modifiedUserInput = userInput.replaceFirst(commandToRemove, BLANK_SPACE);
         return modifiedUserInput.trim();
     }
 
@@ -47,9 +53,13 @@ public class Parser {
         return modifiedString.split(identifier);
     }
 
-    /** Checks for invalid command and throws WalterException */
+    /**
+     * Checks for invalid command and throws WalterException
+     *
+     * @param splitUserInput Original user typed string split by whitespace
+     */
     public static void checkForValidInput(String[] splitUserInput) throws WalterException {
-        if (splitUserInput.length == 1) {
+        if (isValidLength(splitUserInput, INVALID_COMMAND_SIZE)) {
             throw new WalterException(EXCEPTION_EMPTY_FIELD);
         }
     }
@@ -60,7 +70,7 @@ public class Parser {
      * @param field Argument to be checked
      */
     public static void checkForEmptySingleField(String field, String command) throws WalterException {
-        if (field.equals(BLANK_SPACE)) {
+        if (isBlankSpace(field)) {
             switch (command) {
             case "todo":
                 throw new WalterException(EXCEPTION_EMPTY_TODO);
@@ -69,7 +79,7 @@ public class Parser {
             case "schedule":
                 throw new WalterException(EXCEPTION_EMPTY_DATE);
             default:
-                break;
+                throw new WalterException(EXCEPTION_CANNOT_DETERMINE_COMMAND);
             }
         }
     }
@@ -86,34 +96,56 @@ public class Parser {
 
         //Check if both fields have been fulfilled
         for (String information : informationStrings) {
-            if (information.equals(BLANK_SPACE)) {
+            if (isBlankSpace(information)) {
                 fieldsArePresent = false;
                 break;
             }
         }
 
         //Check for valid information
-        if (informationStrings.length < 2 || !fieldsArePresent) {
-            String exceptionMessage = EXCEPTION_TIMEDEVENT_INTRO + command +
-                    EXCEPTION_TIMEDEVENT_BODY +
-                    EXCEPTION_TIMEDEVENT_DESCRIPTION + eventIdentifier + EXCEPTION_TIMEDEVENT_TIMEINFO;
+        if (isInvalidLength(informationStrings, MINIMUM_LENGTH) || !fieldsArePresent) {
+            String exceptionMessage = generateExceptionMessage(command, eventIdentifier);
             throw new WalterException(exceptionMessage);
         }
     }
 
-    /** Returns an arraylist which contains information to replace date in original string */
+    /**
+     * Returns true if length of string array is lesser than the minimum length
+     *
+     * @param splitStrings Array of strings whose length is to be compared with minimum length
+     * @param minimumLength The target minimum length that splitStrings has to be
+     */
+    public static boolean isInvalidLength(String[] splitStrings, int minimumLength) {
+        return splitStrings.length < minimumLength;
+    }
+
+    /**
+     * Generates the exception message for WalterException
+     */
+    public static String generateExceptionMessage(String command, String eventIdentifier) {
+        return EXCEPTION_TIMEDEVENT_INTRO + command +
+                EXCEPTION_TIMEDEVENT_BODY +
+                EXCEPTION_TIMEDEVENT_DESCRIPTION + eventIdentifier + EXCEPTION_TIMEDEVENT_TIMEINFO;
+    }
+
+    /**
+     * Returns an arraylist which contains information to replace date in original string
+     *
+     * @param timeInformation The original field entered by user after the task identifier
+     */
     public static ArrayList<String> determineDateInformation(String timeInformation) {
         String[] splitTimeInformation = timeInformation.split(WHITESPACE_IDENTIFIER);
         ArrayList<String> replacementStrings = new ArrayList<>();
+        Ui ui = new Ui();
 
         //Check if substring contains 2 '-'
         for (String stringInformation : splitTimeInformation) {
             if (stringInformation.contains(HYPHEN_IDENTIFIER)) {
-                if (checkForValidDateFormat(stringInformation)) {
+                if (isValidDateFormat(stringInformation)) {
                     try {
                         replacementStrings = formatDateInformation(stringInformation);
                     } catch (DateTimeParseException e) {
-                        Ui.showInvalidDateFormatError();
+                        ui.showInvalidDateFormatError();
                     }
                     break;
                 }
@@ -122,7 +154,11 @@ public class Parser {
         return replacementStrings;
     }
 
-    /** Formats date object into specified format and returns both original and formatted strings */
+    /**
+     * Formats date object into specified format and returns both original and formatted strings
+     *
+     * @param stringInformation Date information in string form
+     */
     public static ArrayList<String> formatDateInformation(String stringInformation) {
         LocalDate taskDate;
         ArrayList<String> replacementStrings = new ArrayList<>();
@@ -135,26 +171,57 @@ public class Parser {
         return replacementStrings;
     }
 
-    /** Returns true when string contains 3 members separated by 2 hyphens */
-    public static boolean checkForValidDateFormat(String stringInformation) {
+    /**
+     * Returns true when string contains 3 members separated by 2 hyphens
+     *
+     * @param stringInformation Date information in string form
+     */
+    public static boolean isValidDateFormat(String stringInformation) {
         String[] splitDate = stringInformation.split(HYPHEN_IDENTIFIER);
 
         //Check for empty fields
         for (String subString : splitDate) {
-            if (subString.equals(BLANK_SPACE)) {
+            if (isBlankSpace(subString)) {
                 return false;
             }
         }
         //Check for only 3 inputs
-        return splitDate.length == DATE_FORMAT_SIZE;
+        return isValidLength(splitDate, DATE_FORMAT_SIZE);
     }
 
-    /** Returns array of strings from splitting given string with whitespace */
+    /**
+     * Returns true when date input fields have valid length
+     *
+     * @param splitArray Array of strings whose size is to be compared with the
+     * @param targetSize Size that splitArray must be equal to
+     */
+    public static boolean isValidLength(String[] splitArray, int targetSize) {
+        return splitArray.length == targetSize;
+    }
+
+    /**
+     * Returns true when field is empty or white space
+     *
+     * @param subString Current string in array of split dates
+     */
+    public static boolean isBlankSpace(String subString) {
+        return subString.trim().equals(BLANK_SPACE);
+    }
+
+    /**
+     * Returns array of strings from splitting given string with whitespace
+     *
+     * @param userInput Original string input from user
+     */
     public String[] divideUserCommand(String userInput) {
         return userInput.split(WHITESPACE_IDENTIFIER);
     }
 
-    /** Returns command from typed user input */
+    /**
+     * Returns command from typed user input
+     *
+     * @param splitUserInput Original user typed string split by whitespace
+     */
     public String determineCommand(String[] splitUserInput) {
         return splitUserInput[0];
     }
